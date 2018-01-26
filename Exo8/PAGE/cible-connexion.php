552170d99ef9612a87d298781d052c94aff9ad2e
MamaWebsign62 <mregnault.netgyver@gmail.com>
2018-01-26 15:57:47 +0100

    Code commenté

--- Exo8/PAGE/cible-connexion.php
+++ Exo8/PAGE/cible-connexion.php
@@ -10,17 +10,17 @@ if(isset($_POST['connexion'])){ // Je détermine si la variable est définie et 
     $pass = $_POST['pass']; // La variable pass est égale à la valeur du champ pass de la bdd
 
     $sql = 'SELECT * FROM membres_rose_prevost WHERE pseudo = "'.$pseudo.'"'; // Requête pour sélectionner tous les pseudos de la table
-    $req = $bdd->query($sql);
+    $req = $bdd->query($sql); // Je fais une requête vers la bdd
 
-    $count = $req->rowCount();
-    if($count == 1){
-      $result = $req->fetch(PDO::FETCH_ASSOC);;
-      if(password_verify($pass, $result['pass'])){
+    $count = $req->rowCount(); // Je retourne le nombre de résultats de ma requête
+    if($count == 1){ // Si le nbr de résultat est égal à 1
+      $result = $req->fetch(PDO::FETCH_ASSOC); // Je écupère la ligne suivante d'un jeu de résultats PDO
+      if(password_verify($pass, $result['pass'])){ // Je vérifie que le hachage fourni correspond bien au mot de passe fourni
         $_SESSION['connexion_msg'] = "ok";
         $_SESSION['user_pseudo'] = $pseudo;
         $_SESSION['user_id'] = $result['id'];
-      }else{
-        $_SESSION['connexion_msg'] = "error0 : ".$count."//".$sql;
+      }else{ // Sinon
+        $_SESSION['connexion_msg'] = "error0 : ".$count."//".$sql; // Je mets un message d'erreur
       }
     }else{
       $_SESSION['connexion_msg'] = "error1 : ".$count."//".$sql;
--- Exo8/PAGE/getbdd.php
+++ Exo8/PAGE/getbdd.php
@@ -1,10 +1,10 @@
 <?php
 try
-{
+{ // Je fais la liaison avec ma base de données
   $bdd = new PDO('mysql:host=localhost;dbname=espace-connexion;charset=utf8', 'root', 'root',
       array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
-} catch (PDOException $e) {
+} catch (PDOException $e) { // Je vérifie et si tout ne fonctionne pas comme prévu, j'affiche une messge d'erreur
 echo 'Échec lors de la connexion : ' . $e->getMessage();
 }
 ?>
